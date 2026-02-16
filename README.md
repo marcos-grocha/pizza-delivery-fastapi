a3e5058123d2d4b5bfff0c0dc0ac4d2cde2e6a34
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2026-02-16 17:21:04 -0300

    Criando models, banco de dados e primeira migração no alembic

--- README.md
+++ README.md
@@ -1,31 +1,46 @@
-Crie ambiente virtual:
+# Instalação
+### Crie ambiente virtual:
 ```
 python -m venv .venv
 ```
 
-Inicie o ambiente virtual:
+### Inicie o ambiente virtual:
 ```
 .\.venv\Scripts\activate
 ```
 
-Instale as dependências dentro do ambiente:
+### Instale as dependências dentro do ambiente:
 ```
 pip install ...
 ```
 - fastapi
 - uvicorn
 - sqlalchemy
+- sqlalchemy_utils
+- alembic
 - passlib[bcrypt]
 - python-jose[cryptography]
 - python-dotenv
 - python-multipart
 
-Execute o framework com reload automático
+### Execute o framework com reload automático
 ```
 uvicorn main:app --reload
 ```
 
-Consulte a documentação da API gerado pelo FastAPI em:
+### Consulte a documentação da API gerado pelo FastAPI em:
 ```
 http://localhost:8000/docs
 ```
+
+--- 
+
+# Banco de Dados
+### Inicia o alembic
+```alembic init alembic```
+
+### Gera migração
+```alembic revision --autogenerate -m "Mensagem da migração"```
+
+### Roda migração
+```alembic upgrade head```
---
+++ models.py
@@ -0,0 +1,63 @@
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy.orm import declarative_base
+
+# --- Cria a conexão com banco de dados ---
+db = create_engine("sqlite:///banco.db")
+
+# --- Cria a base do banco de dados ---
+Base = declarative_base()
+
+# --- Cria as classes/tabelas do banco ---
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String) # Definindo de maneira explícita
+    email = Column(String, nullable=False)
+    senha = Column(String)
+    ativo = Column(Boolean)
+    admin = Column(Boolean, default=False)
+
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
+
+class Pedido(Base):
+    __tablename__ = "pedidos"
+
+    # STATUS_PEDIDOS = (
+    #     ("PENDENTE", "Pendente"),
+    #     ("CANCELADO", "Cancelado"),
+    #     ("FINALIZADO", "Finalizado")
+    # )
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    usuario = Column(Integer, ForeignKey("usuarios.id"))
+    status = Column(String) # from sqlalchemy_utils.types import ChoiceType
+    preco = Column(Float)
+    # itens
+
+    def __init__(self, usuario, status="PENDENTE", preco=0):
+        self.usuario = usuario
+        self.status = status
+        self.preco = preco
+
+class ItemPedido(Base):
+    __tablename__ = "itens_pedido"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    quantidade = Column(Integer)
+    sabor = Column(String)
+    tamanho = Column(String)
+    preco_unitario = Column(Float)
+    pedido = Column(Integer, ForeignKey("pedidos.id"))
+
+    def __init__(self, quantidade, sabor, tamanho, preco_unitario, pedido):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
