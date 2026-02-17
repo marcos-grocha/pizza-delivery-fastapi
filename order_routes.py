a561b8b6819408dd76ecfd98ef2196fa7721bfd1
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2026-02-17 15:15:37 -0300

    Endpoint: /pedido/cancelar/id

--- order_routes.py
+++ order_routes.py
@@ -1,18 +1,17 @@
-from fastapi import APIRouter, Depends
-from models import Pedido
+from fastapi import APIRouter, Depends, HTTPException
+from models import Pedido, Usuario
 from schemas import PedidoSchema
-from dependencies import pegar_sessao
+from dependencies import pegar_sessao, verificar_token
 from sqlalchemy.orm import Session
 
-# Determina a rota à uma variável + pré caminho do endpoint + marcador para o /docs
-order_router = APIRouter(prefix="/order", tags=["order"])
+# Determina a rota e bloqueia todos os endpoints seguintes
+order_router = APIRouter(prefix="/order", tags=["order"], dependencies=[Depends(verificar_token)])
 
-# Determina a próxima combinação do endpoint
 @order_router.get("/")
 async def pedidos():
     return {"mensagem": "Você acessou a rota de pedidos"}
 
-@order_router.post("/criar_pedido")
+@order_router.post("/pedido")
 async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(pegar_sessao)):
     novo_pedido = Pedido(
         usuario=pedido_schema.usuario,
@@ -22,3 +21,15 @@ async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(p
     session.add(novo_pedido)
     session.commit()
     return {"mensagem": "Você criou um pedido"}
+
+@order_router.get("/pedido/cancelar/{id_pedido}")
+async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
+    pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
+    if not pedido:
+        raise HTTPException(status_code=404, detail="Pedido não encontrado")
+    if not usuario.admin and pedido.usuario != usuario.id:
+        raise HTTPException(status_code=403, detail="Você não tem permissão para cancelar este pedido")
+
+    pedido.status = "CANCELADO"
+    session.commit()
+    return {"mensagem": f"Você cancelou o pedido número {pedido.id}", "pedido": pedido}
