303bd51f1c36b156ab2a484a366476c15362ae73
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2026-02-18 01:40:13 -0300

    Configura a integração ao frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # --- Configurações ---
 ALGORITHM = "HS256"
@@ -7,6 +8,13 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # --- Inicia o Framework ---
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Em produção, o endereço do front
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # --- Define e chama as rotas ---
 from auth_routes import auth_router
